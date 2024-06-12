63495047afeaca6cc25d435e77c98962e3332300
Deborah <86537636+deborah1993@users.noreply.github.com>
2024-06-13 02:47:51 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import * as React from 'react';
-import Map, {NavigationControl} from 'react-map-gl';
-import Navbar from './components/navbar.js';
-import maplibregl from 'maplibre-gl';
-import 'maplibre-gl/dist/maplibre-gl.css';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Hero from "./Hero";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Map mapLib={maplibregl} 
-        initialViewState={{
-          longitude: 16.62662018,
-          latitude: 49.2125578,
-          zoom: 14
-        }}
-        style={{width: "100%", height: " calc(100vh - 77px)"}}
-        mapStyle="https://api.maptiler.com/maps/streets/style.json?key=YOUR_MAPTILER_API_KEY_HERE"
-      >
-        <NavigationControl position="top-left" />
-      </Map>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact Component={Hero}></Route>
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
